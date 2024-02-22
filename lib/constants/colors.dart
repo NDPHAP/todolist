4d5a1476de135ae1f7f1815f6f281713713f2e64
NDPHAP <phap1788@gmail.com>
2024-02-22 19:18:40 +0700

    fix

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color tdRed = Color(0xFFDA4040);
-const Color tdBlue = Color(0xFF5F52EE);
+const Color tdRed = Color(0xFFD43307);
+const Color tdGreen = Color(0xFF17B40C);
 
 const Color tdBlack = Color(0xFF3A3A3A);
 const Color tdGrey = Color(0xFF717171);
